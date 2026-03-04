3952eca11d58925a77195397d81ec742d67eafb4
EC2 Default User <ec2-user@ip-172-31-5-138.ec2.internal>
2026-03-04 17:12:39 +0000

    api-first v1.01.05

--- config/urls.py
+++ config/urls.py
@@ -1,20 +1,18 @@
 from django.contrib import admin
 from django.urls import path
 from core.views import (
-    RegisterOrganizationView,
-    ProviderCreateView,
-    AvailableSlotsView,
-    BookAppointmentView,
-    AppointmentListView,
+    RegisterOrganizationView, 
+    ProviderCreateView, 
+    AvailableSlotsView, 
+    BookAppointmentView, 
+    AppointmentListView
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('api/register/', RegisterOrganizationView.as_view()),
-
-    path('api/providers/', ProviderCreateView.as_view()),
-    path('api/slots/', AvailableSlotsView.as_view()),
-    path('api/book/', BookAppointmentView.as_view()),
-    path('api/appointments/', AppointmentListView.as_view()),
+    path('register/', RegisterOrganizationView.as_view(), name='register'),
+    path('providers/', ProviderCreateView.as_view(), name='providers'),
+    path('slots/', AvailableSlotsView.as_view(), name='slots'),
+    path('book/', BookAppointmentView.as_view(), name='book'),
+    path('appointments/', AppointmentListView.as_view(), name='appointments'),
 ]
